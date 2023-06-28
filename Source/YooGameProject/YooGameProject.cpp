e7769f045cb23ca904ff60ff0a367ebe2b5a026a
jennyuuu010 <115968428+jennyuuu010@users.noreply.github.com>
2023-06-28 17:13:17 +0900

    오류가 발생해서 삭제

--- Source/YooGameProject/YooGameProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "YooGameProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YooGameProject, "YooGameProject" );
